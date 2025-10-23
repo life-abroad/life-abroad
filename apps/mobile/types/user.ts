981b005f581ec0ec43246323d82ca5a833c08da8
nbaker47 <nathan.baker.2647@gmail.com>
2025-10-23 22:07:47 +0800

    chore: icons + user page

--- apps/mobile/types/user.ts
+++ apps/mobile/types/user.ts
@@ -1,6 +1,18 @@
+import { Post } from './post';
+
 export interface User {
   userName: string;
   userHandle: string;
   userAvatar: string;
   profileUrl: string;
+}
+
+export interface UserProfile extends User {
+  friends: User[];
+  circles: {
+    name: string;
+    color: string;
+    users: User[];
+  }[];
+  posts: Post[];
 }
