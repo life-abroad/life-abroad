9bb91e90a2e5ea9b22b37670db7751b84c48736e
nbaker47 <nathan.baker.2647@gmail.com>
2025-10-26 18:34:57 +0800

    chore: web polish

--- apps/mobile/components/CircleBg.tsx
+++ apps/mobile/components/CircleBg.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useResponsive } from 'contexts/ResponsiveContext';
 
 function CircleBg() {
   const { isDesktop } = useResponsive();
 
   return (
-    <ImageBackground
-      source={require('../assets/textures/circle.png')}
-      resizeMode={`repeat`}
-      style={{
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        opacity: isDesktop ? 0.06 : 0.09,
-      }}
-      className={`absolute inset-0 `}
-    />
+    <View style={{ position: 'absolute', width: '100%', height: '100%' }}>
+      <ImageBackground
+        source={require('../assets/textures/circle.png')}
+        resizeMode={isDesktop ? 'repeat' : 'cover'}
+        style={{
+          width: '100%',
+          height: '100%',
+          opacity: isDesktop ? 0.06 : 0.09,
+        }}
+        className={`absolute inset-0 `}
+      />
+      {!isDesktop && (
+        <LinearGradient
+          colors={['rgba(0,0,0,1)', 'rgba(0,0,0,0)']}
+          start={[0, 0]}
+          end={[0, 1]}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      )}
+    </View>
   );
 }
 
