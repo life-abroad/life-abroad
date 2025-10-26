bfdad9e8f602ad3662721c3fa3ada46f704c4593
nbaker47 <nathan.baker.2647@gmail.com>
2025-10-26 17:45:47 +0800

    chore: native polish

--- apps/mobile/components/Blur.native.tsx
+++ apps/mobile/components/Blur.native.tsx
@@ -44,7 +44,7 @@ function Blur({ topBar }: { topBar?: boolean }) {
           colors={['rgba(255, 255, 255, 0.4)', 'rgba(255, 255, 255, 0)']}
           start={{ x: 0, y: 0 }}
           end={{ x: 0, y: 0.3 }}
-          className="absolute bottom-[-3] left-0 right-0 h-1"
+          className="absolute bottom-[-3] left-0 right-0 h-1 opacity-50"
         />
       )}
       {/* Side Edges */}
